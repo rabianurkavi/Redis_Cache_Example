66cf1b6eed7bc4e85d62ad7c6bfcfecd1ff9ed2d
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-11 20:46:05 +0300

    distributed caching

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        [HttpGet("setName")]
+        public async Task<IActionResult> Set(string name, string surName)
+        {
+            await _distributedCache.SetStringAsync("name", name);
+            await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surName));
+            return Ok();
+        }
+        [HttpGet("getName")]
+        public async Task<IActionResult> Get()
+        {
+            var name = await _distributedCache.GetStringAsync("name");
+            var surNameBinary = await _distributedCache.GetAsync("surname");
+            var surName = Encoding.UTF8.GetString(surNameBinary);
+            return Ok(new
+            {
+                name,
+                surName
+            });
+        }
+    }
+}
--- Distributed.Caching/Program.cs
+++ Distributed.Caching/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(options=>options.Configuration = "localhost:9015");
 
 var app = builder.Build();
 
