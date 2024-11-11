0e09a574b0688bed5166dae36e39f1346fdb8468
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-11 20:09:31 +0300

    in memory caching

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        [HttpGet("set/{name}")]
+        public void Set(string name)
+        {     
+            //projeyi çalıştırdığımızda cache e kaydedecek
+            _memoryCache.Set("name", name);
+        }
+        [HttpGet]
+        public string Get()
+        {
+           if(_memoryCache.TryGetValue<string>("name",out string name ))
+            {
+                return name.Substring(3);
+            };
+            return "";
+        }
+    }
+}
