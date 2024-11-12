e55f89b4ee6c7ace41470d947c7318c660396ced
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-12 21:33:55 +0300

    subscribe and publisher

---
+++ Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:9015");
+ISubscriber subscriber = redis.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+Console.Read();
+
