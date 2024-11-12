87cec25c69268937b4ad41f67bcae13a5c575b64
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-13 02:06:51 +0300

    REDİS Pattern-Matching Subscription

--- Redis.Pub.Sub.Subscriber.Example/Program.cs
+++ Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -3,7 +3,7 @@
 ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync("localhost:9015");
 ISubscriber subscriber = redis.GetSubscriber();
 
-await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+await subscriber.SubscribeAsync("mychannel.*", (channel, message) =>
 {
     Console.WriteLine(message);
 });
